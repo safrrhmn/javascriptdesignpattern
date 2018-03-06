af9589c034cb4f23eabc8f6b64ed3f7e6ec70e2a
Saifur Rahman <safrrhmn@hotmail.com>
2018-03-05 22:21:27 -0500

    added 5 cats

--- cat-clicker/cat.js
+++ cat-clicker/cat.js
@@ -1,5 +1,15 @@
+var counter = 0;
 $("button.btnCat").on("click", function() {
   $("div.btnCat").attr("hidden", true);
   var id = $(this).attr("id");
   $("div." + id).removeAttr("hidden");
+  $(".counter").text("Number of count is: 0");
+  counter = 0;
+});
+
+$(".img").on("click", function() {
+  counter += 1;
+  $(this)
+    .siblings(".counter")
+    .html("Number of count is: " + counter);
 });
